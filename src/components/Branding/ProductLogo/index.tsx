21c0f1c11de4c8b2663f4215975f3393e20226a8
niftyspark <167872659+niftyspark@users.noreply.github.com>
2026-01-29 19:14:06 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/components/Branding/ProductLogo/index.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { LobeHub, type LobeHubProps } from '@lobehub/ui/brand';
+import { memo } from 'react';
+
+import { isCustomBranding } from '@/const/version';
+
+import CustomLogo from './Custom';
+
+interface ProductLogoProps extends LobeHubProps {
+  height?: number;
+  width?: number;
+}
+
+export const ProductLogo = memo<ProductLogoProps>((props) => {
+  if (isCustomBranding) {
+    return <CustomLogo {...props} />;
+  }
+
+  return <LobeHub {...props} />;
+});
